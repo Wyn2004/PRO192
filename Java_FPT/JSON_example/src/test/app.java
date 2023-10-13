0d3dc1d008c37f34b50a29534f8f109dfbeb9055
Wyn2004 <thang09052004@gmail.com>
2023-10-13 23:47:10 +0700

    null

--- Java_FPT/JSON_example/src/test/app.java
+++ Java_FPT/JSON_example/src/test/app.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +13,7 @@ import com.google.gson.reflect.TypeToken;
 
 public class app {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		myStudent std1 = new myStudent("Thang", 19, "FPTU");
 		myStudent std2 = new myStudent("Luc", 19, "FPTU");
@@ -37,10 +40,30 @@ public class app {
 		System.out.println(jsonList);
 		
 		// Change JSON text to collection
-		Type listtyType = new TypeToken<List<myStudent>>(){}.getType();
-		List<myStudent> list2 = gson.fromJson(jsonList, listtyType);
+		Type listType = new TypeToken<List<myStudent>>(){}.getType();
+		List<myStudent> list2 = gson.fromJson(jsonList, listType);
 		System.out.println(list2.toString());
 		
-
+		// Push JSON to file
+		FileOutputStream fos = new FileOutputStream("JSONtext.txt");
+		fos.write(jsonList.getBytes());
+		fos.close();
+		
+		// Clone JSON text from file
+		FileReader reader = new FileReader("JSONData.txt");
+		
+		Type objType2 = new TypeToken<myStudent>(){}.getType();
+		myStudent std5 = gson.fromJson(reader, objType);
+		System.out.println(std5.toString());
+		
+		
+		// Clone JSON list from file
+		FileReader reader2 = new FileReader("JSONtext.txt");
+		
+		Type listType2 = new TypeToken<List<myStudent>>(){}.getType();
+		List<myStudent> list3 = gson.fromJson(reader2, listType2);
+		System.out.println(list3.toString());
+		
+		
 	}
 }
